7872b49de108490f984d4de8ebadcc21fa8a5175
wellingtonlopes <ookami.seta@gmail.com>
2020-10-27 17:37:24 -0400

    add input fields to the homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import { Box, Button, Grid, TextField, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { Fragment } from 'react';
+import { Description } from '@material-ui/icons'
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: 100,
+    marginLeft: 50,
+    marginRight: 50,
+    flexGrow: 1,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Fragment>
+      <Box>
+        <Typography
+          className={classes.root}
+          variant="h2"
+          align="center"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Certificados
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center">
+          Preencha os campos abaixo para gerar o seu certificado
+        </Typography>
+      </Box>
+
+      <div className={classes.root}>
+        <Grid
+          container
+          direction="column"
+          alignItems="center"
+          justify="center"
+        >
+          <TextField
+            required
+            id="outlined-required"
+            label="Your name"
+            defaultValue="Full name"
+            variant="outlined"
+            style={{ width: "80%" }}
+            margin="normal"
+          />
+          <TextField
+            required
+            id="outlined-required"
+            label="Your course"
+            defaultValue="Course"
+            variant="outlined"
+            style={{ width: "80%" }}
+            margin="normal"
+          />
+          <Grid item
+            style={{ marginTop: 25 }}>
+            <Button
+              href="#"
+              color="primary"
+              variant="contained"
+              startIcon={<Description />}
+            > Gerar Certificado</Button>
+          </Grid>
+        </Grid>
+      </div>
+    </Fragment>
   );
 }
 
